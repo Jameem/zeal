40c6eded694c2ff90afd40a933e191e07a169f0e
jameem <jameemkurikkal.mp@gmail.com>
2020-03-05 07:59:05 +0530

    code clean up

--- sample-web-app/index.js
+++ sample-web-app/index.js
@@ -1,13 +1,13 @@
-document.querySelector('form').addEventListener('submit', (event)=>{
+document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault()
 
-    const {value} = document.querySelector('input')
+    const { value } = document.querySelector('input')
 
     const header = document.querySelector('h1')
-    if(value.includes('@')){
-        header.innerHTML='Looks good!'
+    if (value.includes('@')) {
+        header.innerHTML = 'Looks good!'
     }
-    else{
-        header.innerHTML='Invalid email!'
+    else {
+        header.innerHTML = 'Invalid email!'
     }
 })
