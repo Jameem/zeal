a4c23066f3c37b88f2866bce67da6771651b652e
jameem <jameemkurikkal.mp@gmail.com>
2020-03-06 07:51:47 +0530

    accessing render in runner

--- runner.js
+++ runner.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
+const render = require('./render')
 
 const forbiddenDirs = ['node_modules']
 
@@ -13,13 +14,16 @@ class Runner {
         for (let file of this.testFiles) {
             console.log(chalk.grey(`----${file.shortName}`))
             const beforeEaches = []
+            global.render = render
+            
             global.beforeEach = (fn) => {
                 beforeEaches.push(fn)
             }
-            global.it = (desc, fn) => {
+
+            global.it = async (desc, fn) => {
                 beforeEaches.forEach(func => func())
                 try {
-                    fn()
+                    await fn()
                     console.log(chalk.green(`\tOK - ${desc}`))
                 } catch (err) {
                     const message = err.message.replace(/\n/g, '\n\t\t')
@@ -27,6 +31,7 @@ class Runner {
                     console.log(chalk.red('\t', message))
                 }
             }
+
             try {
                 require(file.name)
             } catch (err) {
